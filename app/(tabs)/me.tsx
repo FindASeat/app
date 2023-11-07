b841248d0e94d4a9527983c4b9c3f4be7a291e8b
Rohan Kalra <rohankal@usc.edu>
2023-11-06 21:24:11 -0800

    added some past reservations

--- app/(tabs)/me.tsx
+++ app/(tabs)/me.tsx
@@ -20,6 +20,69 @@ const Me = () => {
     setReservations(updatedReservations);
   }
 
+  const pastReservations = [
+  {
+    code: "LVL",
+    seat: "inside-4-6",
+    start: "11/5",
+    end: "10:00 AM",
+  },
+  {
+    code: "JFF",
+    seat: "outside-2-3",
+    start: "10/31",
+    end: "12:00 AM",
+  },
+    {
+    code: "VCE",
+    seat: "inside-4-6",
+    start: "10/23",
+    end: "5:00 PM",
+  },
+  {
+    code: "JFF",
+    seat: "inside-2-5",
+    start: "10/14",
+    end: "4:00 PM",
+  },
+    {
+    code: "THH",
+    seat: "outside-2-1",
+    start: "10/5",
+    end: "5:00 PM",
+  },
+  {
+    code: "LVL",
+    seat: "outside-2-3",
+    start: "9/28",
+    end: "3:00 PM",
+  },
+  {
+    code: "LVL",
+    seat: "inside-2-1",
+    start: "9/14",
+    end: "10:00 AM",
+  },
+  {
+    code: "LVL",
+    seat: "outside-2-3",
+    start: "9/6",
+    end: "4:00 PM",
+  },
+  {
+    code: "LVL",
+    seat: "outside-0-0",
+    start: "8/29",
+    end: "3:30 PM",
+  },
+  {
+    code: "LVL",
+    seat: "inside-2-1",
+    start: "8/15",
+    end: "2:00 PM",
+  },
+];
+
     useFocusEffect(
       React.useCallback(() => {
         const fetchUserData = async () => {
@@ -44,6 +107,17 @@ const Me = () => {
             key={index}
             reservation={reservation}
             onCancel={cancelAndFetchReservations}
+            showCancel={true}
+          />
+        ))}
+      <Text style={styles.title}>Past Reservations:</Text>
+      {/* <Button title="Test Button" onPress={() => testFunction()} /> */}
+        {pastReservations.map((reservation, index) => (
+          <ReservationBubble
+            key={index}
+            reservation={reservation}
+            onCancel={cancelAndFetchReservations}
+            showCancel={false}
           />
         ))}
       </ScrollView>
