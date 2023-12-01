e3c64ba130247c9c312473ca2a145f58835d47ca
Brendon Zimmer <brendonzimmerbusiness@gmail.com>
2023-11-30 21:19:46 -0800

    Add tab bar icons

--- app/(stack)/(tabs)/_layout.tsx
+++ app/(stack)/(tabs)/_layout.tsx
@@ -1,17 +1,29 @@
 import LogoutButton from '../../../components/LogoutButton';
 import { useGlobal } from '../../../context/GlobalContext';
+import Icon from 'react-native-vector-icons/MaterialCommunityIcons';
 import { Tabs } from 'expo-router';
 
 const tab_layout = () => {
   const { user } = useGlobal();
 
-  // TODO change icons
   return (
     <Tabs screenOptions={{ tabBarActiveTintColor: '#990000', tabBarStyle: { display: user ? 'flex' : 'none' } }}>
-      <Tabs.Screen name="(map)" options={{ headerShown: false, tabBarLabel: 'Reserve' }} />
+      <Tabs.Screen
+        name="(map)"
+        options={{
+          headerShown: false,
+          tabBarLabel: 'Reserve',
+          tabBarIcon: ({ color, size }) => <Icon name="office-building-marker" color={color} size={size} />,
+        }}
+      />
       <Tabs.Screen
         name="profile"
-        options={{ tabBarLabel: 'Profile', headerTitle: 'Profile', headerRight: LogoutButton }}
+        options={{
+          tabBarLabel: 'Profile',
+          headerTitle: 'Profile',
+          headerRight: LogoutButton,
+          tabBarIcon: ({ color, size }) => <Icon name="account" color={color} size={size} />,
+        }}
       />
     </Tabs>
   );
